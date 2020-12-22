1cce74db91799c2fb0e61c94ff910c94fd24722e
saggers <sagarparmar0233@gmail.com>
2020-12-21 23:13:28 -0800

    git ignore added

---
+++ src/main/java/com/springboot/reactAstronauts/SpringbootBackendApiForAstronautsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.reactAstronauts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApiForAstronautsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApiForAstronautsApplication.class, args);
+	}
+
+}
